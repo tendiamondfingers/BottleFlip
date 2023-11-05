dbfc0ef814d513267f33b9ad07168ba332ba4f26
Sergey Zakharov <sergey_sentybr@mail.ru>
2023-11-05 14:38:33 +0200

    create items plist

---
+++ BottleFlip/Bottle.swift
@@ -0,0 +1,8 @@
+//
+//  Bottle.swift
+//  BottleFlip
+//
+//  Created by Сергей Захаров on 05.11.2023.
+//
+
+import Foundation
---
+++ BottleFlip/BottleController.swift
@@ -0,0 +1,8 @@
+//
+//  BottleController.swift
+//  BottleFlip
+//
+//  Created by Сергей Захаров on 05.11.2023.
+//
+
+import Foundation
